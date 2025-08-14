39d809bb71697ef682f9bb013f4a2941e127f203
fabioBM-Dev <faleobamu@gmail.com>
2025-08-14 07:00:11 -0500

    Crea funcion agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigosAgregados = [];
+console.log(amigosAgregados)
+
+function agregarAmigo() {
+    document.getElementById("amigo");
+    if(document.getElementById("amigo").value =""){
+        alert("Por favor, ingrese unnombre");
+    } else {
+        amigosAgregados.push(document.getElementById("amigo").value)
+    }
+}
