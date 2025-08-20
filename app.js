9ed8d3091db33b1c3fc5b8a58f66da7312c7cfb7
fabioBM-Dev <faleobamu@gmail.com>
2025-08-20 03:51:26 -0500

    restaurado real funcion lista()

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosAgregados = [];
-console.log(amigosAgregados)
 
 function agregarAmigo() {
     document.getElementById("amigo");
-    if(document.getElementById("amigo").value =""){
-        alert("Por favor, ingrese unnombre");
+    if(document.getElementById("amigo").value ==""){
+        alert("Por favor ingrese un nombre");
     } else {
-        amigosAgregados.push(document.getElementById("amigo").value)
+        amigosAgregados.push(document.getElementById("amigo").value);
     }
+     document.getElementById("amigo").value = "";
 }
+
+function lista(){
+    //seleccionar elmento <ul>
+    const amigos = document.querySelector("listaAmigos");
+    
+    //recorrer el arreglo usando "for"
+        amigosAgregados.forEach (nombreAmigo => {
+       //Crear un nuevo elemento <li>
+            const itemLista = document.createElement("li");
+        //asignar el texto de la array a la lista 
+            itemLista.textContent = amigos;
+        //agregar el nuevo <li> a la <ul>
+            amigos.appendChild(itemLista);
+        //
+        });
+        lista()
+   }
+
+  
