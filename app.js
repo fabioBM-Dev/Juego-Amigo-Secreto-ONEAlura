8d1870b7b3b5b197c66eff1f8c0c57af859b6c14
fabioBM-Dev <faleobamu@gmail.com>
2025-08-20 03:26:03 -0500

    se restatura version original funcion lista()

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigosAgregados = [];
+console.log(amigosAgregados)
 
 function agregarAmigo() {
     document.getElementById("amigo");
-    if(document.getElementById("amigo").value ==""){
-        alert("Por favor ingrese un nombre");
+    if(document.getElementById("amigo").value =""){
+        alert("Por favor, ingrese unnombre");
     } else {
-        amigosAgregados.push(document.getElementById("amigo").value);
+        amigosAgregados.push(document.getElementById("amigo").value)
     }
-     document.getElementById("amigo").value = "";
 }
